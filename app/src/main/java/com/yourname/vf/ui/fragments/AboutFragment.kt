97205a6f8bec87af09b9bdb4a11bcc5f293cf21e
temporary-2731 <temporary25209@gmail.com>
2026-07-25 14:23:25 -0300

    Add AboutFragment for displaying about information

---
+++ app/src/main/java/com/yourname/vf/ui/fragments/AboutFragment.kt
@@ -0,0 +1,10 @@
+package com.yourname.vf.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.yourname.vf.R
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {}
+}
