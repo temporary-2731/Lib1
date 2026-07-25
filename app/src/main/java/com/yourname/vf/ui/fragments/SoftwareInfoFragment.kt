06a0e3e5767babe284216ace82826704880e26ae
temporary-2731 <temporary25209@gmail.com>
2026-07-25 14:24:11 -0300

    Add SoftwareInfoFragment for software info display

---
+++ app/src/main/java/com/yourname/vf/ui/fragments/SoftwareInfoFragment.kt
@@ -0,0 +1,10 @@
+package com.yourname.vf.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.yourname.vf.R
+
+class SoftwareInfoFragment : Fragment(R.layout.fragment_software_info) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {}
+}
