42f1080af395a8626e62eb8cf0ddbd1684c8d49d
temporary-2731 <temporary25209@gmail.com>
2026-07-25 13:37:30 -0300

    Implement drawQuad function for OpenGL rendering
    Added a drawQuad function to render a textured quad using OpenGL.

--- app/src/main/java/com/yourname/vf/media/GlUtil.kt
+++ app/src/main/java/com/yourname/vf/media/GlUtil.kt
@@ -1,9 +1,9 @@
 package com.yourname.vf.media
 
 import android.opengl.GLES20
-import android.opengl.GLUtils
-import android.graphics.SurfaceTexture
-import android.view.Surface
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 
 object GlUtil {
     fun createTexture(): Int {
@@ -15,5 +15,29 @@ object GlUtil {
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
         return textures[0]
+    }
+
+    fun drawQuad(shader: V360Shader) {
+        val vertices = floatArrayOf(
+            -1f, -1f, 0f, 0f,
+             1f, -1f, 1f, 0f,
+            -1f,  1f, 0f, 1f,
+             1f,  1f, 1f, 1f
+        )
+        val buffer = ByteBuffer.allocateDirect(vertices.size * 4)
+            .order(ByteOrder.nativeOrder()).asFloatBuffer()
+        buffer.put(vertices).position(0)
+
+        val posLoc = GLES20.glGetAttribLocation(shader.program, "aPosition")
+        val texLoc = GLES20.glGetAttribLocation(shader.program, "aTexCoord")
+        GLES20.glEnableVertexAttribArray(posLoc)
+        GLES20.glVertexAttribPointer(posLoc, 2, GLES20.GL_FLOAT, false, 16, buffer)
+        buffer.position(2)
+        GLES20.glEnableVertexAttribArray(texLoc)
+        GLES20.glVertexAttribPointer(texLoc, 2, GLES20.GL_FLOAT, false, 16, buffer)
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+        GLES20.glDisableVertexAttribArray(posLoc)
+        GLES20.glDisableVertexAttribArray(texLoc)
     }
 }
