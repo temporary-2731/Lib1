bab6bfb769e6bcf293b8363792e9984089a2bbd4
temporary-2731 <temporary25209@gmail.com>
2026-07-25 13:09:20 -0300

    Add GlUtil object for OpenGL texture creation

---
+++ app/src/main/java/com/yourname/vf/media/GlUtil.kt
@@ -0,0 +1,19 @@
+package com.yourname.vf.media
+
+import android.opengl.GLES20
+import android.opengl.GLUtils
+import android.graphics.SurfaceTexture
+import android.view.Surface
+
+object GlUtil {
+    fun createTexture(): Int {
+        val textures = IntArray(1)
+        GLES20.glGenTextures(1, textures, 0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0])
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+        return textures[0]
+    }
+}
